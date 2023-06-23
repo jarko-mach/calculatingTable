1b8ab12cb0410fd800f627e02ed9d8d7e905d3e9
Jarko Mach <jarko.mach@gmail.com>
2023-06-23 09:03:12 +0200

    button 'dodaj nowa linia' is adding textarea inputs for different dates

--- index.js
+++ index.js
@@ -13,40 +13,29 @@ const tableData = [
         }
     }]
 
-console.log("1", tableData[0]?.details.illumination())
+// console.log("1", tableData[0]?.details.illumination())
 
-let element = document.querySelector('.container2')
-element.style.gridTemplateAreas = " lp miejsce zmierzone eksploatacyjne eksploatacyjne rownomiernosc rownomiernosc zgodnosc"
-" lp miejsce zmierzone eksploatacyjne1 eksploatacyjne2 rownomiernosc1 rownomiernosc2 zgodnosc"
-" myNumber1 myNumber2 myNumber3 myNumber4 myNumber5 myNumber6 myNumber7 myNumber8 ";
+const element = document.querySelector('.container')
 
-
-const addNewDataLine = () => {
-
+const addLine = () => {
     for (let i = 1; i < 9; i++) {
-        const newParagraph = document.createElement('p')
-        newParagraph.classList.add(`"item-data${i}"`)
-        newParagraph.classList.add("item-data")
-        newParagraph.innerText = `dane${i}`
-        element.append(newParagraph)
+        const newElement = document.createElement('textarea')
+        newElement.classList.add(`"item-data${i}"`)
+        newElement.classList.add("item-data")
+        // newElement.setAttribute("type", "button")
+        // newElement.setAttribute("maxlength",3)
+        // newParagraph.innerText = `dane${i}`
+        element.append(newElement)
     }
-    element.style.gridTemplateAreas += "data1 data2 data3 data4 data5 data6 data7 data8"
     return "dodałem linię"
 }
 
-console.log(addNewDataLine())
+const createLine = () => {
+    addLine()
+}
 
-// newLine.innerHTML = `
-//     <p class="item-data1">dana1</p>
-//     <p class="item-data2">dana2</p>
-//     <p class="item-data3">dana3</p>
-//     <p class="item-data4">dana4</p>
-//     <p class="item-data5">dana5</p>
-//     <p class="item-data6">dana6</p>
-//     <p class="item-data7">dana7</p>
-//     <p class="item-data8">dana8</p>
-//     `
+const buttoNewLine = document.querySelector(".buttonAdd")
+buttoNewLine.addEventListener('click', createLine)
 
-// lastElement.after(newLine)
 
-// let addNewInfo = document.querySelector('.addNewData')
+
