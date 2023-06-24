e473c4f0d2fdc74b90ab0a5531b4c5ddecf1ebab
Jarko Mach <jarko.mach@gmail.com>
2023-06-24 11:17:56 +0200

    Almost is going, but now I am thinking about changing to html table

--- index.js
+++ index.js
@@ -31,31 +31,34 @@ const dataTypes = [
     },
     {
         element: "textarea",
+        value: "824, 878, 760, 742, 786, 788",
         class1: "item-data-zmierzone",
         class2: "item-data",
     },
     {
-        element: "textarea",
+        element: "p",
         class1: "item-data-eksploatacyjne1",
         class2: "item-data",
     },
     {
-        element: "textarea",
+        element: "input",
+        type: "number",
         class1: "item-data-eksploatacyjne2",
         class2: "item-data",
     },
     {
-        element: "textarea",
+        element: "p",
         class1: "item-data-rownomiernosc1",
         class2: "item-data",
     },
     {
-        element: "textarea",
+        element: "input",
+        type: "number",
         class1: "item-data-rownomiernosc2",
         class2: "item-data",
     },
     {
-        element: "textarea",
+        element: "p",
         class1: "item-data-zgodnosc",
         class2: "item-data",
     },
@@ -72,9 +75,14 @@ const addLine = () => {
         let newElement = document.createElement(dataTypes[i - 1].element)
 
         if (dataTypes[i - 1].element === "input") {
-            console.log("1", dataTypes[i - 1].element)
             newElement.setAttribute("type", dataTypes[i - 1].type)
             newElement.setAttribute("value", dataTypes[i - 1].value)
+        }
+
+        if (dataTypes[i - 1].element === "textarea") {
+            console.log("1", dataTypes[i - 1].element)
+            newElement.setAttribute("type", dataTypes[i - 1].type)
+            
         }
 
         newElement.classList.add(dataTypes[i - 1].class1)
@@ -82,7 +90,10 @@ const addLine = () => {
         elementTable.append(newElement)
 
         if (dataTypes[i - 1].element === "button") {
-            newElement = document.querySelectorAll(".recalc").forEach(item => item.innerText = "Przelicz")
+            document.querySelectorAll(".recalc").forEach((item) => {
+                item.innerText = "Przelicz"
+                item.addEventListener('click', recalc)
+            })
         }
     }
 }
@@ -91,10 +102,31 @@ const createNextLine = () => {
     addLine()
 }
 
+const recalc = (e) => {
+    let calculatedEksploatacyjne
+    let calculatedRownomiernosc
+    let measurings = document.querySelector(".item-data-zmierzone").value
+
+    const re = /,/;
+    measureTable = measurings.split(re);
+    measureTable.forEach((elem, index) => { measureTable[index] = Number(measureTable[index]) })
+
+    const computeEksploatacyjne = () => {
+        let average = 0
+        measureTable.forEach((elem) => { average += elem })
+        calculatedEksploatacyjne = Number(Math.round(average / measureTable.length + 'e+0') + 'e-0')
+        return calculatedEksploatacyjne
+    }
+
+    document.querySelector(".item-data-eksploatacyjne1").innerText = computeEksploatacyjne()
+
+    document.querySelector(".item-data-rownomiernosc1").innerText
+        = Number(Math.round(Math.min(...measureTable) / calculatedEksploatacyjne + 'e+2') + 'e-2')
+}
+
 const elementTable = document.querySelector('.container')
 
 const buttoNewLine = document.querySelector("#buttonAdd")
 buttoNewLine.addEventListener('click', createNextLine)
 
-
-
+// console.log("1", dataTypes[i - 1].element)
