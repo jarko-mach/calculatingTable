59124a48b086919534616a50027830cae2c83d54
Jarko Mach <jarko.mach@gmail.com>
2023-07-01 10:37:41 +0200

    It seems that the problem with the width of the columns has been solved.

--- app/tableParamets.js
+++ app/tableParamets.js
@@ -1,20 +1,20 @@
 // szerokości kolumn tabeli
-let tableParameters = [7, 50, 45, 18, 12, 15, 12, 12]
-let converter = 3
-let convertedTableParameters = tableParameters.map(element => element * converter);
+// let tableParameters = [7, 50, 45, 18, 12, 15, 12, 12]
+// let converter = 3
+// let convertedTableParameters = tableParameters.map(element => element * converter);
 
 export const addTableNumbers = () => {
     const lastElement = document.querySelector(".showNumbers")
     lastElement.innerHTML =
         ` <tr>
-        <td class="tableNumber" width="${convertedTableParameters[0]}mm">1</td>
-        <td class="tableNumber" width="${convertedTableParameters[1]}mm">2</td>
-        <td class="tableNumber" width="${convertedTableParameters[2]}mm">3</td>
-        <td class="tableNumber" width="${convertedTableParameters[3]}mm">4</td>
-        <td class="tableNumber" width="${convertedTableParameters[4]}mm">5</td>
-        <td class="tableNumber" width="${convertedTableParameters[5]}mm">6</td>
-        <td class="tableNumber" width="${convertedTableParameters[6]}mm">7</td>
-        <td class="tableNumber" width="${convertedTableParameters[7]}mm">8</td>
+        <td class="tableGeneral tableNumber cell007mm">1</td>
+        <td class="tableGeneral tableNumber cell050mm">2</td>
+        <td class="tableGeneral tableNumber cell045mm">3</td>
+        <td class="tableGeneral tableNumber cell016mm">4</td>
+        <td class="tableGeneral tableNumber cell012mm">5</td>
+        <td class="tableGeneral tableNumber cell016mm">6</td>
+        <td class="tableGeneral tableNumber cell012mm">7</td>
+        <td class="tableGeneral tableNumber cell012mm">8</td>
         </tr>
     `
 }
@@ -24,31 +24,24 @@ export const createTableNewLine = () => {
     let newElement = document.createElement("tr")
     newElement.classList.add("addedLine")
 
-    // let szeroko1 = document.querySelector(".showNumbers").firstElementChild
-    // szeroko1 = document.querySelector(".showNumbers")
-    // console.log("szerokości:", szeroko1)
+    // <td class="cells" width="4%">a</td>
+    // <td class="cells" width="29%">b</td>
+    // <td class="cells" width="26%">b</td>
+    // <td class="cells" width="11%">b</td>
+    // <td class="cells" width="7%">b</td>
+    // <td class="cells" width="9%">b</td>
+    // <td class="cells" width="7%">b</td>
+    // <td class="cells" width="7%">b</td>
 
     newElement.innerHTML =
-        ` <td> <input class="numberLp cell1" type="number" value="1"></td>
-          <td> <textarea class="place cell2" width="10mm"></textarea> </td>
-          <td> <textarea class="measurings cell3"></textarea> </td>
-          <td> <input type="number" class="wynik-1 cell4" disabled></td>
-          <td> <input type="number" class="norma-1 cell5"></td>
-          <td> <input type="number" class="wynik-2 cell6" disabled></td>
-          <td> <input type="number" class="norma-2 cell7" step="0.1"></td>
-          <td> <input type="text" class="compatibility cell8" disabled></td>
+        ` <td class="tableGeneral"> <input class="numberLp cellInput" type="number" value="1"></td>
+          <td class="tableGeneral"> <textarea class="place cellInput"></textarea> </td>
+          <td class="tableGeneral"> <textarea class="measurings cellInput"></textarea> </td>
+          <td class="tableGeneral"> <input type="number" class="wynik-1 cellInput" disabled></td>
+          <td class="tableGeneral"> <input type="number" class="norma-1 cellInput"></td>
+          <td class="tableGeneral"> <input type="number" class="wynik-2 cellInput" disabled></td>
+          <td class="tableGeneral"> <input type="number" class="norma-2 cellInput" step="0.1"></td>
+          <td class="tableGeneral"> <input type="text" class="compatibility cellInput" disabled></td>
     `
     lastElement.append(newElement)
 }
-
-
-// stara wersja
-// ` <td> <input class="cellWidth-small numberLp" type="number" value="1"></td>
-// <td> <textarea rows="4" cols="20" class="place"></textarea> </td>
-// <td> <textarea rows="4" cols="30" class="measurings"></textarea> </td>
-// <td> <input type="number" class="wynik-1 cellWidth-small" disabled></td>
-// <td> <input type="number" class="cellWidth-small norma-1"></td>
-// <td> <input type="number" class="wynik-2 cellWidth-small" disabled></td>
-// <td> <input type="number" class="cellWidth-small norma-2" step="0.1"></td>
-// <td> <input type="text" class="compatibility cellWidth-small" disabled></td>
-// `
